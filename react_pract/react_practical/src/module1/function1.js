a8805ddb686e34f9fe777676af8c10092d33fcac
hemantpatel21 <hemant21patel@gmail.com>
2024-03-22 22:00:23 +0530

    assign 21-03

---
+++ react_pract/react_practical/src/module1/function1.js
@@ -0,0 +1,2 @@
+export const name = "Hemant patel";
+export const age = 34;
---
+++ react_pract/react_practical/src/module1/index.js
@@ -0,0 +1,3 @@
+import {name, age} from "./function1.js";
+console.log("my name is ", name);
+console.log("my age is ", age);
