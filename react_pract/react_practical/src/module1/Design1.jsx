a3ddab211284a9a0c6729d0d5a1e651ef010063b
hemantpatel21 <hemant21patel@gmail.com>
2024-03-28 02:38:24 +0530

    asingment

---
+++ react_pract/react_practical/src/module1/Design1.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../css/Design1.css'
+
+function Design1()
+{
+
+    return( <div>
+        <div class="logo">
+            LOGO
+        </div>
+        <div class="nav">
+            Navigation
+        </div>
+        <div class="header">
+            Header part
+        </div>
+        <div class="sidebarleft">
+            side bar
+        </div>
+        <div class="bodyarea">
+            Body Area
+        </div>
+        <div class="footer">
+            Footer
+        </div>
+        </div>
+    );
+    //return <h1>from design1</h1>
+
+}
+
+export default Design1;
---
+++ react_pract/react_practical/src/module1/T1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+function T1()
+{
+    return <h1>T1 hello world T1 a</h1>;
+}
+
+export default T1;
